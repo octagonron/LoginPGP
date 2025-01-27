800bd27b575b7ad7dd706e46962731d476843171
octagonron <67324884+octagonron@users.noreply.github.com>
2025-01-26 18:29:52 -0600

    Create LoginForm.js

---
+++ LoginForm.js
@@ -0,0 +1,8 @@
+// Change background
+className="min-h-screen bg-cover bg-center"
+
+// Modify input fields
+className="w-full bg-black/50 border border-green-500/30"
+
+// Update button styles
+className="w-full bg-black/50 text-white"
